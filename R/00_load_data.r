7b36ee31d233f86ffd96f4c8624a33476c1a9751
s223102 <s223102@student.dtu.dk>
2024-01-17 11:31:28 +0100

    add cell_category

--- R/00_load_data.r
+++ R/00_load_data.r
@@ -1,5 +1,20 @@
 # Load data
 
-covid_nbt_main <- readRDS("/net/pupil1/home/people/s223102/scRNAseq/212bab45-4c1a-476f-839b-8116c482cbcf/covid_nbt_main.rds")
-covid_nbt_loc <- readRDS("/net/pupil1/home/people/s223102/scRNAseq/ae93c0aa-c97c-4cd9-9ab1-e9a44c85eec4/covid_nbt_loc.rds")
+install.packages("Seurat")
+BiocManager::install("BiocSingular")
+BiocManager::install("scDblFinder")
+BiocManager::install("SingleCellExperiment")
+BiocManager::install("SingleR")
+BiocManager::install("scater")
+library(Seurat)
+library(data.table)
+library(ggplot2)
+library(dplyr)
+library(BiocSingular)
+library(scDblFinder)
+library(SingleCellExperiment)
+library(SingleR)
+library(scater)
+
+covid_nbt_main <- readRDS("data/covid_sub10.rds")
 viral_load <- readxl::read_excel("/net/pupil1/home/people/s223102/scRNAseq/a6fb8098-7a58-45e6-884e-f25b1cdb94dd/ViralRNA_CPM.xlsx")
